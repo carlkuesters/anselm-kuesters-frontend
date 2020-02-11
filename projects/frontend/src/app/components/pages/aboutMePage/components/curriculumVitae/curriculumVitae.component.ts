fba05536806a5e1c1f083eca18e1f0b85762d311
carlkuesters <carl.kuesters@youengineering.com>
2020-02-11 11:13:36 +0100

    Update curriculum vitae.

--- projects/frontend/src/app/components/pages/aboutMePage/components/curriculumVitae/curriculumVitae.component.ts
+++ projects/frontend/src/app/components/pages/aboutMePage/components/curriculumVitae/curriculumVitae.component.ts
@@ -10,11 +10,13 @@ import {AboutMeEvent} from '../../classes/aboutMeEvent';
 export class CurriculumVitaeComponent {
   // TODO: Receive from backend
   events: AboutMeEvent[] = [
-    <AboutMeEvent>{date: '2018', text: 'M.Phil - Economic and Social History', location: 'University of Oxford', major: true},
-    <AboutMeEvent>{date: '2016', text: 'B.Sc. - Economics', location: 'Ruprecht-Karls-Universität Heidelberg', major: true},
-    <AboutMeEvent>{date: '2015', text: 'Auslandssemester', location: 'University of Copenhagen', major: false},
-    <AboutMeEvent>{date: '2015', text: 'B.A. - Geschichte und Germanistik', location: 'Ruprecht-Karls-Universität Heidelberg', major: true},
-    <AboutMeEvent>{date: '2011', text: 'Abitur', location: 'Faust-Gymnasium Staufen', major: false},
-    <AboutMeEvent>{date: '1992', text: 'Geburt', location: 'Freiburg im Breisgau', major: true}
+    {date: 'Seit 2018', text: 'Promotion', location: 'MPI für Eur. Rechtsgeschichte Frankfurt', major: false},
+    {date: 'Seit 2018', text: 'LL.M. - Europäisches Recht', location: 'Julius-Maximilians-Universität Würzburg', major: false},
+    {date: '2018', text: 'M.Phil. - Economic and Social History', location: 'University of Oxford', major: true},
+    {date: '2016', text: 'B.Sc. - Economics', location: 'Ruprecht-Karls-Universität Heidelberg', major: true},
+    {date: '2015', text: 'Auslandssemester', location: 'University of Copenhagen', major: false},
+    {date: '2015', text: 'B.A. - Geschichte und Germanistik', location: 'Ruprecht-Karls-Universität Heidelberg', major: true},
+    {date: '2011', text: 'Abitur', location: 'Faust-Gymnasium Staufen', major: false},
+    {date: '1992', text: 'Geburt', location: 'Freiburg im Breisgau', major: true}
   ];
 }
