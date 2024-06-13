998551ba292ea712c2db5037ef34ce222e808ebf
Carl Küsters <carl.kuesters@gmail.com>
2024-06-14 01:47:24 +0200

    Container
    Container

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,6 +1,6 @@
 describe('AnselmKuestersApp', () => {
   it('should display copyright', () => {
     cy.visit('/')
-    cy.get('.copyright').contains('Copyright © 2023 Anselm Küsters')
+    cy.get('.copyright').contains('Copyright © 2024 Anselm Küsters')
   })
 })
